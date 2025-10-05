7aa277c9d5418ccceea8edf2a609df8eec767b0b
sahan-chinthaka <sahanchinthaka04@gmail.com>
2025-10-05 14:06:44 +0530

    Format the codebase

---
+++ global.d.ts
@@ -0,0 +1,8 @@
+// Ambient module declarations for style imports
+// This lets TypeScript accept side-effect imports like `import './globals.css'`.
+declare module "*.css";
+declare module "*.scss";
+declare module "*.sass";
+declare module "*.module.css";
+declare module "*.module.scss";
+declare module "*.module.sass";
